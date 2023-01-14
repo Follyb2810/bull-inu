8eecac7647e0b9afc35d50334bb596226a5d66b7
Babatunde Yusuf Folorunsho <babatundeyusuffolorunsho@gmail.com>
2023-01-14 15:41:58 +0100

    adding few

--- Components/Header.js
+++ Components/Header.js
@@ -5,8 +5,9 @@ export default function Header(){
         <header>
            {/* <p>Minting is Now closed</p> */}
            {/* <div> */}
-                <Image  src={Logo} height={50} width={50} className={"logo"}/>
-                <span><i className="fa-brands fa-telegram fa-2x"></i><br/>Join Telegram</span>
+                <Image  src={Logo} height={50}   width={50} className={"logo"} alt="footerImg"/>
+                <span><a href="https://t.me/BULLISH_INU"><i className="fa-brands fa-telegram fa-2x"></i><br/>Join Telegram</a></span>
+                {/* <span><a href="https://mail.google.com/mail/u/0/#inbox"><i className="fa-brands fa-telegram fa-2x"></i><br/>Join Telegram</a></span> */}
                 {/* <span><i className="fa-brands fa-twitter fa-2x"></i><br/>Join Twitter</span> */}
                 {/* <button>Connect</button> */}
            {/* </div> */}
--- Components/Main.js
+++ Components/Main.js
@@ -7,8 +7,12 @@ export default function Main(){
                 At wall Street  Bull was launched. With a top speed of 5857 mph, the spaceship ascended to a height of 157 miles. Bull inu face was in focus as an automated camera was mounted inside the capsule.
                         Bull Inu was in fair condition after the travel, albeit a little weary and dehydrate
             </p>
+            <p>Based on the early 2023 hype and bull run, we are creating this token to celebrate the bull season.
+The long awaited bull is now here, cheers to the bull run 🥂
+
+The team behind this are all experienced and connected, buckle up your seat belt as we ride along the Bullish trend </p>
             <ol>
-                <li>Coin Symbol: BUll INU</li>
+                <li>Coin Symbol: BULL INU</li>
                 <li>Total Supply:10,000,000,000Tokens</li>
                 <li>Liquidity: Locked & Burned</li>
                 <li>  Holder Distribution: 5% of each transaction is redistributed to Bull INU holders; one can earn more by simply holding the token</li> 
